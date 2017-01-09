18c8c335c0a3f21f4687823d7b738ebc0e8be116
Zach Almon <z.almon@yahoo.com>
2017-01-09 08:53:33 -0600

    Update products.php

--- PHP/Database Web Store/products.php
+++ PHP/Database Web Store/products.php
@@ -141,9 +141,9 @@ elseif (!isset($_COOKIE[$cookie_name]) && $CookiesEnabled) {
 <?php
 
 //Contains Servername, the username, and the pasword of the MySQL server we will use
-$UK_servername = "delphi.cs.uky.edu";
-$My_username = "czal222";
-$My_password = "u0613596";
+$UK_servername = "";
+$My_username = "";
+$My_password = "";
 
 //connection string
 $connection = new mysqli($UK_servername, $My_username, $My_password);
@@ -154,7 +154,7 @@ if ($connection->connect_error) {
 }
 
 //select database to use
-$connection->select_db("czal222");
+$connection->select_db("");
 
 
 

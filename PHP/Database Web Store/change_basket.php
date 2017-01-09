dc8255cd3128f1e50d2446181b9761c831f3ab2c
Zach Almon <z.almon@yahoo.com>
2017-01-09 08:50:07 -0600

    Update change_basket.php

--- PHP/Database Web Store/change_basket.php
+++ PHP/Database Web Store/change_basket.php
@@ -145,9 +145,9 @@ $ItemsNewQuantity = $_POST["New_Quantity"];
 
 //Checks for login ------------------------------------------
 //Contains Servername, the username, and the pasword of the MySQL server we will use
-$UK_servername = "delphi.cs.uky.edu";
-$My_username = "czal222";
-$My_password = "u0613596";
+$UK_servername = "";
+$My_username = "";
+$My_password = "";
 
 //connection string
 $connection = new mysqli($UK_servername, $My_username, $My_password);
